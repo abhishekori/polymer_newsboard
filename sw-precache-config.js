5fc604683a08240b97419b8d2aac1c63bc9145e5
abhishek <abhikori1994@gmail.com>
2017-01-21 23:04:57 +0530

    last few changes

---
+++ sw-precache-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    staticFileGlobs: [
+        '/index.html',
+        '/manifest.json',
+        '/bower_components/webcomponentsjs/webcomponents-lite.js'
+    ],
+    navigateFallback: '/index.html',
+    navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+    runtimeCaching: [
+        {
+            urlPattern: /^https:\/\/i\.ytimg\.com/,
+            handler: 'fastest',
+            options: {
+                cache: {
+                    maxEntries: 100,
+                    name: 'yt-images-cache'
+                }
+            }
+        },
+        {
+            urlPattern: /^https:\/\/www\.googleapis\.com\/youtube\/v3\/search/,
+            handler: 'networkFirst',
+            options: {
+                cache: {
+                    maxEntries: 100,
+                    name: 'yt-data-cache'
+                }
+            }
+        }
+    ]
+};/**
+ * Created by abhishek on 21/1/17.
+ */

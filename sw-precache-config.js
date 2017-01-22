ab62ab5441cd2312d7db7248156fd1ea3231c0af
abhishek <abhikori1994@gmail.com>
2017-01-22 14:48:06 +0530

    last changes

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     staticFileGlobs: [
-        '/index.html',
+        //'/index.html',
         '/manifest.json',
         '/bower_components/webcomponentsjs/webcomponents-lite.js'
     ],
